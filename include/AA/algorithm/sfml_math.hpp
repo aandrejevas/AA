5670994b5d35ad8c4a759f494fb66dbc776de68f
AAndrejevas <aandrejevas5@gmail.com>
2022-06-16 10:08:19 +0300

    sf namespace

--- include/AA/algorithm/sfml_math.hpp
+++ include/AA/algorithm/sfml_math.hpp
@@ -16,7 +16,7 @@ namespace std {
 	template<class T>
 	struct tuple_size<sf::Vector2<T>> : std::integral_constant<size_t, 2> {};
 
-	template<size_t I, class T>
+	template<size_t I, aa::arithmetic T>
 		requires (I < std::tuple_size_v<sf::Vector2<T>>)
 	struct tuple_element<I, sf::Vector2<T>> : std::type_identity<T> {};
 
@@ -40,52 +40,58 @@ namespace sf {
 			return v.y;
 	}
 
-}
 
-namespace aa {
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
+	AA_CONSTEXPR sf::Vector2<T> operator-(const sf::Vector2<T> &l, const T r) { return {l.x - r, l.y - r}; }
+
+	template<aa::arithmetic T>
+	AA_CONSTEXPR sf::Vector2<T> operator+(const sf::Vector2<T> &l, const T r) { return {l.x + r, l.y + r}; }
+
+
+
+	template<aa::arithmetic T>
 	AA_CONSTEXPR bool intersects(const sf::Rect<T> &a, const sf::Rect<T> &b) {
 		return !((b.left + b.width <= a.left) || (a.left + a.width <= b.left) || (b.top + b.height <= a.top) || (a.top + a.height <= b.top));
 	}
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR bool contains(const sf::Rect<T> &a, const sf::Rect<T> &b) {
 		return (a.left <= b.left) && (b.left + b.width <= a.left + a.width) && (a.top <= b.top) && (b.top + b.height <= a.top + a.height);
 	}
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR bool contains(const sf::Rect<T> &a, const sf::Vector2<T> &b) {
 		return (a.left <= b.x) && (b.x < a.left + a.width) && (a.top <= b.y) && (b.y < a.top + a.height);
 	}
 
 
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR T dist_sq(const sf::Vector2<T> &a) {
-		return sq(a.x) + sq(a.y);
+		return aa::sq(a.x) + aa::sq(a.y);
 	}
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR T dist_sq(const sf::Vector2<T> &a, const sf::Vector2<T> &b) {
-		return sq(a.x - b.x) + sq(a.y - b.y);
+		return aa::sq(a.x - b.x) + aa::sq(a.y - b.y);
 	}
 
 
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR T dist(const sf::Vector2<T> &a) {
 		return std::hypot(a.x, a.y);
 	}
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR T dist(const sf::Vector2<T> &a, const sf::Vector2<T> &b) {
 		return std::hypot(a.x - b.x, a.y - b.y);
 	}
 
 
 
-	template<arithmetic T>
+	template<aa::arithmetic T>
 	AA_CONSTEXPR T dot(const sf::Vector2<T> &a, const sf::Vector2<T> &b) {
 		return (a.x * b.x) + (a.y * b.y);
 	}
