4af8ff0f8f60c74f8f069032ac5355f87bc3b4eb
AAndrejevas <aandrejevas5@gmail.com>
2022-07-30 23:05:01 +0300

    created new create function

--- include/AA/algorithm/init.hpp
+++ include/AA/algorithm/init.hpp
@@ -4,6 +4,9 @@
 #include <cstddef> // size_t
 #include <cstring> // memcpy, memset
 #include <type_traits> // remove_const_t
+#include <concepts> // invocable
+#include <functional> // invoke
+#include <utility> // forward
 #include <memory> // addressof
 
 
@@ -23,6 +26,13 @@ namespace aa {
 	[[gnu::always_inline]] AA_CONSTEXPR std::remove_const_t<D> create_with_memset(const int v, const size_t n) {
 		std::remove_const_t<D> d;
 		std::memset(std::addressof(d), v, n);
+		return d;
+	}
+
+	template<trivially_default_constructible D, std::invocable<D &> F>
+	[[gnu::always_inline]] AA_CONSTEXPR std::remove_const_t<D> create_with_lambda(F &&f) {
+		std::remove_const_t<D> d;
+		std::invoke(std::forward<F>(f), d);
 		return d;
 	}
 
